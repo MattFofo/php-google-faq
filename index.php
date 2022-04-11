2eed73fec4d8b9585a0a93d415386d9e8fa19308
MattFofo <matteo.segattini2@gmail.com>
2022-04-11 15:30:08 +0200

    versione base

--- index.php
+++ index.php
@@ -24,7 +24,19 @@
         ],
         [
             'question' => 'Perché il mio account è associato a un paese?',
-            'answer' => ''
+            'answer' => "
+            <p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p>
+            <ul>
+                <li>
+                    La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                    <ul>
+                        <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                        <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+                    </ul>
+                </li>
+                <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+            </ul>
+            <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>"
         ],
         [
             'question' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
